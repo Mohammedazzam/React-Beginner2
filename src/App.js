180a15bbba5d96e0d98b05132399f3409a57f321
Mohammed Azzam <mohammed.mojahed93@gmail.com>
2022-03-02 00:33:20 +0200

    (class component & function component)الفرق بين

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      Hello Mohammed Azzam
-    </div>
-  );
+// function App() {
+//   return (
+//     <div className="App">
+//       Hello Mohammed Azzam
+//     </div>
+//   );
+// }
+
+// export default App;
+
+
+
+import React, {Component} from "react";
+
+class App extends Component{
+  render() {
+    return (
+      <div className="App">
+        Hello Mohammed Azzam
+      </div>
+    );
+  }
 }
 
 export default App;
