0d7b1a7590c6c8c0a2713b650f5bb7cca49b324d
Mohammed Azzam <mohammed.mojahed93@gmail.com>
2022-03-05 14:38:29 +0200

    (Updating && unMounting)شرح مرحلة ال

--- src/component/Home.js
+++ src/component/Home.js
@@ -1,24 +1,49 @@
 import React, { Component } from 'react'
 
 class Home extends Component {
-    constructor(props){
+    constructor(props) {
         super(props);
         console.log("constructor")
         this.state = {
-        
+            name: 'Home'
         }
+    }
+    updateDate = () => {
+        this.setState({
+            name: "Home Component"
+        })
     }
     static getDerivedStateFromProps(props, state) {
         console.log("getDerivedStateFromProps")
         return null;
     }
+    shouldComponentUpdate(nextProps, nextState) {
+        console.log("shouldComponentUpdate")
+        return true;
+    }
+    getSnapshotBeforeUpdate(prevProps, prevState) {
+        console.log("getSnapshotBeforeUpdate")
+    }
+    componentDidUpdate(prevProps, prevState) {
+        console.log("componentDidUpdate")
+    }
+
+
+    componentWillUnmount() {
+        console.log("componentWillUnmount")
+    }
+
+
     componentDidMount() {
         console.log("componentDidMount")
     }
     render() {
         console.log("render")
         return (
-            <div>Home</div>
+            <div>
+                Home
+                <button onClick={this.updateDate}>Update</button>
+            </div>
         )
     }
 }
